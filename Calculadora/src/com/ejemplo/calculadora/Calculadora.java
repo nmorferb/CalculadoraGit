f89b58685c502bbce1d3231da367a0499fd9c788
Nerea Moreno <nmorferb@g.educaand.es>
2025-10-23 12:18:15 +0200

    Versión 4: agregar división; añadida operación de división con control de error.

--- Calculadora/src/com/ejemplo/calculadora/Calculadora.java
+++ Calculadora/src/com/ejemplo/calculadora/Calculadora.java
@@ -10,4 +10,10 @@ public double restar(double a, double b) {
 public double multiplicar(double a, double b) {
 	 return a * b;
 	}
+public double dividir(double a, double b) {
+	 if (b == 0) {
+	 throw new IllegalArgumentException("No se puede dividir entre cero");
+	 }
+	 return a / b;
+	}
 }
